9431deec8154ef5ff517372d9187784f161d3aea
Pol Quintana <polete2802@gmail.com>
2023-05-24 19:45:44 +0200

    Edit funcional

--- resources/views/editespacio.blade.php
+++ resources/views/editespacio.blade.php
@@ -10,20 +10,24 @@
         @csrf
         @method('put')
 <div class="mb-3">
+    <label for="id" class="form-label">ID</label>
+    <input type="text" class="form-control" id="id" disabled value="{{ $espacio->id }}" name="id">
+</div>
+<div class="mb-3">
     <label for="nombre" class="form-label">Nombre del espacio</label>
-    <input type="text" class="form-control" id="nombre" value="{{ $espacio->nombre }}">
+    <input type="text" class="form-control" id="nombre" value="{{ $espacio->nombre }}" name="nombre">
 </div>
 <div class="mb-3">
     <label for="area" class="form-label">Area</label>
-    <input type="text" class="form-control" id="area" value="{{ $espacio->area }}">
+    <input type="text" class="form-control" id="area" value="{{ $espacio->area }}" name="area">
 </div>
 <div class="mb-3">
     <label for="capacidad" class="form-label">Capacidad</label>
-    <input type="datetime" class="form-control" id="capacidad" value="{{ $espacio->capacidad }}">
+    <input type="datetime" class="form-control" id="capacidad" value="{{ $espacio->capacidad }}" name="capacidad">
 </div>
 <div class="mb-3">
     <label for="disponibilidad" class="form-label">Disponibilidad</label>
-    <input type="text" class="form-control" id="disponibilidad" value="{{ $espacio->disponibilidad }}">
+    <input type="text" class="form-control" id="disponibilidad" value="{{ $espacio->disponibilidad }}" name="disponibilidad">
 </div>
 
 <button type="submit" class="btn btn-primary">Submit</button>
