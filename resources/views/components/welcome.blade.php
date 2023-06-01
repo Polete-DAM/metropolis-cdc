cf4aad433eef56ff85e6244f24d4c462e6ba4d1e
Pol Quintana <polete2802@gmail.com>
2023-06-02 00:59:54 +0200

    calendario agregado de vuelta

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -35,11 +35,85 @@
 				<div class="col-md-6">
 					
 				<p>Bienvenido a tu dashboard</p>
-
 				</div>
 				<div class="col-md-9">
-				
-					
+					<p>Calendario de Reservas</p>
+					<p> JUNIO 2023 </p>
+				</div>
+				<div class="col-md-9">
+					<table class="table">
+  						<thead>
+    						<tr>
+      							<th scope="col">SUN</th>
+      							<th scope="col">MON</th>
+      							<th scope="col">TUE</th>
+      							<th scope="col">WED</th>
+      							<th scope="col">THU</t>
+      							<th scope="col">FRI</th>
+      							<th scope="col">SAT</th>
+   							 </tr>
+  						</thead>
+ 					 	<tbody>
+    						<tr>
+      							<td>28</td>
+      							<td>29</td>
+      							<td>30</td>
+      							<td>31</td>
+      							<td>1</td>
+      							<td>2</td>
+      							<td>3</td>
+    						</tr>
+    						<tr>
+      							<td>4</td>
+      							<td>5</td>
+      							<td>6</td>
+      							<td>7</td>
+      							<td>8</td>
+      							<td>9</td>
+      							<td>10</td>
+      							    						
+      						</tr>
+    						<tr>
+      							<td>4</td>
+      							<td>5</td>
+      							<td>6</td>
+      							<td>7</td>
+      							<td>8</td>
+      							<td>9</td>
+      							<td>10</td>
+      							
+    						</tr>
+    						<tr>
+      							<td>11</td>
+      							<td>12</td>
+      							<td>13</td>
+      							<td>14</td>
+      							<td>15</td>
+      							<td>16</td>
+      							<td>17</td>
+      					
+    						</tr>
+    						<tr>
+      							<td>18</td>
+      							<td>19</td>
+      							<td>20</td>
+      							<td>21</td>
+      							<td>22</td>
+      							<td>23</td>
+      							<td>24</td>
+    						</tr>
+    						<tr>
+      							<td>25</td>
+      							<td>26</td>
+      							<td>27</td>
+      							<td>28</td>
+      							<td>29</td>
+      							<td>30</td>
+      							
+    						</tr>
+    						
+  						</tbody>
+					</table>
     				
 				</div>
 			</div>
